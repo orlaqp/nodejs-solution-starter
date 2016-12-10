2dc87cd583422fe68232a3018b1209405c906e08
Orlando Quero <orlaqp@Orlandos-MacBook-Pro-2.local>
2016-12-10 09:23:47 -0500

    made a great progress on the new definition of what an activity is supposed to be

--- lib/enforcer/activity.ts
+++ lib/enforcer/activity.ts
@@ -1,12 +1,70 @@
 import * as _ from 'lodash';
 import { IIdentity } from '../../data/models';
 import { IPermission } from '../rbac'
+import * as Promise from 'bluebird';
 
-export interface Activity {
-    may: String;
-    hasPermissions?: IPermission[];
-    when?(identity: IIdentity, cb: (err: any, authorized: Boolean) => void);
+/** 
+ * Defines the data returned when an activity is validated before its execution
+ */
+export interface IValidationResult {
+    errors: any;
+    success: boolean;
 }
+
+/** 
+ * This interface represents an activity definition.
+ * An activity is a way to define a set of rules for validation authorization and implementation
+ * of a specific task
+*/
+export interface Activity<T> {
+
+    /** 
+     * Activity name
+     * */
+    name: string;
+
+    /** 
+     * An optional description for your activity
+     * @optional
+     */
+    description?: string;
+
+    /**
+     * A set of permissions required by the user who is executing the activity
+     */
+    hasPermissions?: IPermission[];
+
+    /**
+     * If you require a complex authorization mechanism for an activity
+     * that goes beyond permissions you can use this method to specify your rules.
+     * This method can be as complex as you need it to be, just make sure you return a promise
+     */
+    when?(identity: IIdentity, cb: (err: any, authorized: Boolean) => void);
+
+    /**
+     * Method called only when authorization is succesful to validate 
+     * if the input data for the activity is valid
+     */
+    validate(): Promise<IValidationResult>
+
+    /**
+     * Executes the logic for this activity
+     */
+    execute(): Promise<T>;
+
+    /**
+     * Specifies if an attempt to execute this activity should be logged
+     */
+    log: boolean;
+
+    /**
+     * Specifies if the changes on this activity should be audit and saved
+     * to the database so the changes can audited at a later time
+     */
+    audit: boolean;
+}
+
+
 
 export class ActivityCollection {
     private _activities: Activity[];
